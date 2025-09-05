b27f3902e577561aa1c169846affb279fcd9efa5
riya-046 <riyashinde046@gmail.com>
2025-09-05 14:07:00 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
 smart_resume_analyzer/
 │── .venv/              # already created
-│── resumes/            # put sample resume PDFs here
+│── resumes/            # put sample resume in te xt formathere
 │── skills.json         # list of skills to check
 │── analyzer.py         # main script
+|── job.txt             # put job description
+|── resume_analysis.csv # open in excel 
+  
+  
+  
